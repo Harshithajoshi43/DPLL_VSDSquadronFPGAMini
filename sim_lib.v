85a478d9c16ce6af4acffcd476f12146676a9f71
Harshitha R Joshi <harshithajoshi43@gmail.com>
2025-05-06 22:58:36 +0530

    Create sim_lib.v

---
+++ sim_lib.v
@@ -0,0 +1,8 @@
+  GNU nano 7.2                                                                                                      sim_lib.v                                                                                                                
+// sim_lib.v — stub for the Lattice SB_GB primitive
+module SB_GB (
+    input  USER_SIGNAL_TO_GLOBAL_BUFFER,
+    output GLOBAL_BUFFER_OUTPUT
+);
+  assign GLOBAL_BUFFER_OUTPUT = USER_SIGNAL_TO_GLOBAL_BUFFER;
+endmodule
